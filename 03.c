9a32e5315be7263628867a748f1ed905c23e7b9c
Anveshagarg172 <anveshagargofficial@gmail.com>
2026-06-20 12:36:32 +0530

    Day_01

---
+++ 03.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int n;long long fact=1;
+    printf("enter number :");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)fact*=i;
+    printf("factorial:%lld",fact);
+    return 0;
+}
---
+++ 04.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int n,count=0;
+    printf("enter number:");
+    scanf("%d",&n);
+    while (n !=0){ n/=10;count++;}
+    printf("Digits: %d,count");
+    return 0;
+}
